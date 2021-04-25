d90000a47eef5029c7d6a4d764f7a6aa45041ca6
Reinazhard <reinazhard@gmail.com>
2021-04-26 01:02:48 +0700

    use my gcc
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- kernel.sh
+++ kernel.sh
@@ -127,7 +127,7 @@ COMMIT_HEAD=$(git log --oneline -1)
 	echo " "
 		msg "|| Cloning GCC 10 baremetal ||"
 		git clone --depth=1 https://github.com/silont-project/aarch64-elf-gcc -b arm64/10 gcc64
-		git clone --depth=1 https://github.com/arter97/arm32-gcc gcc32
+		git clone --depth=1 https://github.com/silont-project/arm-eabi-gcc -b arm/10 gcc32
 		GCC64_DIR=$KERNEL_DIR/gcc64
 		GCC32_DIR=$KERNEL_DIR/gcc32
 
