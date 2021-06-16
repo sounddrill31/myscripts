e615d2b58d71fa06760d55c4963a1a7e84aff29b
Reinazhard <reinazhard@gmail.com>
2021-06-16 13:44:52 +0000

    use LLD for linker
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- kernel.sh
+++ kernel.sh
@@ -216,7 +216,7 @@ build_kernel() {
 
 	msg "|| Started Compilation ||"
 	export CROSS_COMPILE_ARM32=$GCC32_DIR/bin/arm-eabi-
-	make -j"$PROCS" O=out CROSS_COMPILE=aarch64-elf-
+	make -j"$PROCS" O=out CROSS_COMPILE=aarch64-elf- LD=ld.lld
 
 		BUILD_END=$(date +"%s")
 		DIFF=$((BUILD_END - BUILD_START))
