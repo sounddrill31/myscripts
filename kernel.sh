0504b022ba63ebc671855890970bc7de9afba701
Reinazhard <reinazhard@gmail.com>
2021-06-20 23:09:22 +0700

    Dont announce build
    no need to fucking spam
    
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- kernel.sh
+++ kernel.sh
@@ -140,11 +140,6 @@ tg_post_build() {
 ##----------------------------------------------------------##
 
 build_kernel() {
-	if [ "$PTTG" = 1 ]
- 	then
-		tg_post_msg "<b>🔨 $KBUILD_BUILD_VERSION CI Build Triggered</b>%0A<b>Kernel Version : </b><code>$KERVER</code>%0A<b>Date : </b><code>$(TZ=Asia/Jakarta date)</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0a<b>Branch : </b><code>$CI_BRANCH</code>%0A<b>Top Commit : </b><a href='$DRONE_COMMIT_LINK'>$COMMIT_HEAD</a>" "$CHATID"
-	fi
-
 	msg "|| Started Compilation ||"
 	BUILD_START=$(date +"%s")
 	make O=out $DEFCONFIG
