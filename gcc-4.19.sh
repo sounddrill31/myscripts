6d5e2f36d4b75f41cd73529fa176f51704b184ee
Reinazhard <reinazhard@gmail.com>
2021-11-23 15:19:43 +0700

    Fix kernel compiler string
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- gcc-4.19.sh
+++ gcc-4.19.sh
@@ -87,7 +87,7 @@ exports() {
 	export ARCH=arm64
 	export SUBARCH=arm64
 
-	KBUILD_COMPILER_STRING=$("$GCC64_DIR"/bin/aarch64-elf-gcc --version | head -n 1)
+	KBUILD_COMPILER_STRING=$("$GCC64_DIR"/bin/aarch64*-elf-gcc --version | head -n 1)
 	PATH=$GCC64_DIR/bin/:$GCC32_DIR/bin/:/usr/bin:$PATH
 
 	export CROSS_COMPILE_COMPAT=$GCC32_DIR/bin/arm-none-eabi-
