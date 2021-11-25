bf1e3338baa40a679bc58bf0b18170086f69e2ad
Reinazhard <reinazhard@gmail.com>
2021-11-25 20:59:40 +0700

    Add script for tulip-4.19
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

---
+++ tulip-4.19.sh
@@ -0,0 +1,163 @@
+#! /bin/bash
+
+ # Script For Building Android arm64 Kernel
+ #
+ # Copyright (c) 2018-2020 Panchajanya1999 <rsk52959@gmail.com>
+ #
+ # Licensed under the Apache License, Version 2.0 (the "License");
+ # you may not use this file except in compliance with the License.
+ # You may obtain a copy of the License at
+ #
+ #      http://www.apache.org/licenses/LICENSE-2.0
+ #
+ # Unless required by applicable law or agreed to in writing, software
+ # distributed under the License is distributed on an "AS IS" BASIS,
+ # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ # See the License for the specific language governing permissions and
+ # limitations under the License.
+ #
+
+#Kernel building script
+
+# Function to show an informational message
+msg() {
+    echo -e "\e[1;32m$*\e[0m"
+}
+
+err() {
+    echo -e "\e[1;41m$*\e[0m"
+    exit 1
+}
+
+##------------------------------------------------------##
+##----------Basic Informations, COMPULSORY--------------##
+
+# The defult directory where the kernel should be placed
+KERNEL_DIR=$PWD
+
+# The name of the Kernel, to name the ZIP
+ZIPNAME="SiLonT-4nineteen"
+
+# The codename of the device
+DEVICE="tulip"
+
+# The defconfig which should be used. Get it from config.gz from
+# your device or check source
+DEFCONFIG=vendor/tulip_defconfig
+
+##------------------------------------------------------##
+##---------Do Not Touch Anything Beyond This------------##
+
+## Set defaults first
+CI_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+token=$TELEGRAM_TOKEN
+export KBUILD_BUILD_HOST CI_BRANCH
+
+## Export CI Env
+export KBUILD_BUILD_VERSION=$DRONE_BUILD_NUMBER
+export CI_BRANCH=$DRONE_BRANCH
+export CHATID="-1001403511595"
+
+#Check Kernel Version
+KERVER=$(make kernelversion)
+
+
+# Set a commit head
+COMMIT_HEAD=$(git log --oneline -1)
+
+#Now Its time for other stuffs like cloning, exporting, etc
+
+ clone() {
+	echo " "
+		msg "|| Cloning GCC ||"
+		git clone --depth=1 --single-branch https://github.com/pjorektneira/arm64-gcc.git -b arm gcc64
+		git clone --depth=1 --single-branch https://github.com/pjorektneira/arm32-gcc.git -b arm gcc32
+		GCC64_DIR=$KERNEL_DIR/gcc64
+		GCC32_DIR=$KERNEL_DIR/gcc32
+
+	msg "|| Cloning Anykernel ||"
+	git clone --depth 1 --no-single-branch https://github.com/Reinazhard/AnyKernel3.git -b tulip
+}
+
+##------------------------------------------------------##
+
+exports() {
+	export KBUILD_BUILD_USER="reina"
+	export KBUILD_BUILD_HOST="Laptop-Sangar"
+	export ARCH=arm64
+	export SUBARCH=arm64
+
+	KBUILD_COMPILER_STRING=$("$GCC64_DIR"/bin/aarch64*-elf-gcc --version | head -n 1)
+	PATH=$GCC64_DIR/bin/:$GCC32_DIR/bin/:/usr/bin:$PATH
+
+	export CROSS_COMPILE_COMPAT=$GCC32_DIR/bin/arm-none-eabi-
+	export CROSS_COMPILE=$GCC64_DIR/bin/aarch64-none-elf-
+	export PATH KBUILD_COMPILER_STRING
+	export BOT_MSG_URL="https://api.telegram.org/bot$token/sendMessage"
+	export BOT_BUILD_URL="https://api.telegram.org/bot$token/sendDocument"
+	PROCS=$(($(nproc --all) + 2))
+	export PROCS
+}
+
+##---------------------------------------------------------##
+
+tg_post_msg() {
+	curl -s -X POST "$BOT_MSG_URL" -d chat_id="$CHATID" \
+	-d "disable_web_page_preview=true" \
+	-d "parse_mode=html" \
+	-d text="$1"
+
+}
+
+##----------------------------------------------------------------##
+
+tg_post_build() {
+	#Show the Checksum alongwith caption
+	curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
+	-F chat_id="$2"  \
+	-F "disable_web_page_preview=true" \
+	-F "parse_mode=html" \
+	-F caption="$3 | <code>Build Number : </code><b>$DRONE_BUILD_NUMBER</b>"
+}
+
+##----------------------------------------------------------##
+
+build_kernel() {
+
+	msg "|| Started Compilation ||"
+	BUILD_START=$(date +"%s")
+	make O=out $DEFCONFIG
+	make -j"$PROCS" O=out
+	BUILD_END=$(date +"%s")
+	DIFF=$((BUILD_END - BUILD_START))
+
+	if [ -f "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb ]
+	    then
+	    	msg "|| Kernel successfully compiled ||"
+		gen_zip
+	else
+		tg_post_msg "<b>❌ Build failed to compile after $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds</b>" "$CHATID"
+	fi
+
+}
+
+##--------------------------------------------------------------##
+
+gen_zip() {
+	msg "|| Zipping into a flashable zip ||"
+	cp "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb AnyKernel3/Image.gz-dtb
+
+	cd AnyKernel3 || exit
+	zip -r9 $ZIPNAME-$DEVICE-"$DRONE_BUILD_NUMBER" ./* -x .git README.md
+
+	## Prepare a final zip variable
+	ZIP_FINAL="$ZIPNAME-$DEVICE-$DRONE_BUILD_NUMBER.zip"
+	tg_post_build "$ZIP_FINAL" "$CHATID" "✅ Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
+	cd ..
+}
+
+clone
+exports
+build_kernel
+
+##----------------*****-----------------------------##
